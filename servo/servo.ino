55d2f17cbd40990154e45416e047bc084b7b8aaa
ALEXJAZZ008008 <alexandercwhitehead@gmail.com>
2018-08-18 23:26:59 +0100

    removed code reuse

--- servo/servo.ino
+++ servo/servo.ino
@@ -2,68 +2,75 @@
 
 Servo servo;
 
+const int update_frequency = 30;
 const int angle_start = 10;
-const int angle_end = 170;
-const int delay_amount_sync = 1000 / 30;
+const int angle_end = 150;
 const int delay_amount_start = 1000;
-const int delay_amount_process = 10;
+const int delay_amount_process = 1;
+const int delay_amount_sync = 1000 / update_frequency;
 
 int i = 0;
 int time_elapsed = 0;
+
+int servo_move(int servo_angle)
+{
+  time_elapsed = 0;
+  
+  servo.write(servo_angle);
+
+  while(servo.read() != servo_angle)
+  {
+    time_elapsed += delay_amount_process;
+    
+    delay(delay_amount_process);
+  }
+
+  return time_elapsed;
+}
+
+int servo_init()
+{
+  delay(delay_amount_start - servo_move(angle_start));
+
+  return 1;
+}
 
 void setup()
 {
   servo.attach(9);
 
-  servo.write(angle_start);
-
-  time_elapsed = 0;
-
-  while(servo.read() != angle_start)
-  {
-    delay(delay_amount_process);
-
-    time_elapsed += delay_amount_process;
-  }
-
-  delay(delay_amount_start - time_elapsed);
+  servo_init();
 }
 
-void loop()
+int servo_forwards()
 {
   for(i = angle_start; i < angle_end; ++i)
   {
     servo.write(i);
-
-    time_elapsed = 0;
-
-    while(servo.read() != i)
-    {
-      delay(delay_amount_process);
-
-      time_elapsed += delay_amount_process;
-    }
     
-    delay(delay_amount_sync - time_elapsed);
+    delay(delay_amount_sync - servo_move(i));
   }
+
+  return 1;
+}
+
+int servo_backwards()
+{
+  for(i = angle_start; i < angle_end; ++i)
+  {
+    delay(delay_amount_sync - servo_move(angle_end - i));
+  }
+
+  return 1;
+}
+
+void loop()
+{
+  servo_forwards();
 
   delay(delay_amount_start);
   
-  for(i = angle_start; i < angle_end; ++i)
-  {
-    servo.write(angle_end - i);
-
-    time_elapsed = 0;
-
-    while(servo.read() != (angle_end - i))
-    {
-      delay(delay_amount_process);
-
-      time_elapsed += delay_amount_process;
-    }
-    
-    delay(delay_amount_sync - time_elapsed);
-  }
+  servo_backwards();
 
   delay(delay_amount_start);
 }
